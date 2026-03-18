9bcf7047e82baacfcc18b9b2bb2e84f271735708
MateuszN949 <mateusz.nowicki@onet.eu>
2026-03-18 23:30:00 +0100

    Finish LINQ Task 1

--- 03-LINQ/GoldSavings.App/DataServices/GoldAnalysisService.cs
+++ 03-LINQ/GoldSavings.App/DataServices/GoldAnalysisService.cs
@@ -17,5 +17,60 @@ namespace GoldSavings.App.Services
         {
             return _goldPrices.Average(p => p.Price);
         }
+
+        public List<(int, double)> GetAveragePriceFromYears(IEnumerable<int> years)
+        {
+            return [..
+                from goldPrice in _goldPrices
+                where years.Contains(goldPrice.Date.Year)
+                group goldPrice by goldPrice.Date.Year into gr
+                select (gr.Key, gr.Average(p => p.Price))];
+        }
+
+        public List<GoldPrice> GetTopPricesMethod(int num, bool highest = true)
+        {
+            return GetTopPricesMethod(new Range(0, num), highest);
+        }
+        public List<GoldPrice> GetTopPricesMethod(Range range, bool highest = true)
+        {
+            return [.. _goldPrices.OrderBy(gp => highest ? -gp.Price : gp.Price).Take(range)];
+        }
+
+        public List<GoldPrice> GetTopPricesQuery(int num, bool highest = true)
+        {
+            return GetTopPricesQuery(new Range(0, num), highest);
+        }
+        public List<GoldPrice> GetTopPricesQuery(Range range, bool highest = true)
+        {
+            IEnumerable<GoldPrice> goldPrices =
+                from goldprice in _goldPrices
+                orderby (highest ? -goldprice.Price : goldprice.Price)
+                select goldprice;
+
+            return [.. goldPrices.Take(range)];
+        }
+
+        public List<(GoldPrice, List<GoldPrice>)> FindBenefitableDates(double returnRatio)
+        {
+            return [..
+                from buyDate in _goldPrices
+                let sellDates =
+                    (from sellDate in _goldPrices
+                    where sellDate.Date > buyDate.Date && sellDate.Price > returnRatio * buyDate.Price
+                    select sellDate).ToList()
+                where sellDates.Count != 0
+                select (buyDate, sellDates)];
+        }
+
+        public (GoldPrice, GoldPrice, double) FindBestInvestment()
+        {
+            return (from buyDate in _goldPrices
+            let bestSell =
+                (from sellDate in _goldPrices
+                where sellDate.Date > buyDate.Date
+                select sellDate).MaxBy(sd => sd.Price)
+            where bestSell != null
+            select (buyDate, bestSell, (bestSell.Price - buyDate.Price) / buyDate.Price)).OrderByDescending(i => i.Item3).FirstOrDefault();
+        }
     }
 }
--- 03-LINQ/GoldSavings.App/Model/GoldPrice.cs
+++ 03-LINQ/GoldSavings.App/Model/GoldPrice.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using System.Xml.Serialization;
 
 namespace GoldSavings.App.Model;
 
@@ -9,4 +10,14 @@ public class GoldPrice
 
     [JsonProperty("Cena")]
     public double Price { get; set; }
+
+    public static void SaveToXml(List<GoldPrice> prices, string filePath)
+{
+    var serializer = new XmlSerializer(typeof(List<GoldPrice>));
+
+        using var writer = new StreamWriter(filePath);
+        serializer.Serialize(writer, prices);
+    }
+
+    public static List<GoldPrice> LoadFromXml(string filePath) => (List<GoldPrice>)new XmlSerializer(typeof(List<GoldPrice>)).Deserialize(new StreamReader(filePath));
 }
